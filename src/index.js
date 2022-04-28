daee8e6678a668ee7750fd8ac1c9d40fbaf77499
Konst Konst <dragone555777@gmail.ru>
2022-04-28 23:46:25 +0500

    Константин Чеботарев

--- src/index.js
+++ src/index.js
@@ -36,3 +36,82 @@ const post = {
     return comment;
   }),
 };
+
+document.querySelector('.mainImg').src = post.pathToPostImg;
+post.tags.forEach(x => {
+  const tag = document.createElement('a');
+  tag.textContent = x;
+  document.querySelector('.tags').appendChild(tag);
+})
+document.querySelector('.title').textContent = post.title;
+document.querySelector('.date').textContent = post.date.toLocaleDateString('ru-Ru');
+document.querySelector('.descText').textContent = post.description;
+document.querySelector('.likeCount').textContent = post.likes;
+document.querySelector('.commentCount').textContent = post.comments.length;
+
+function createComment(comment) {
+  const commentBlock = document.createElement('div');
+  commentBlock.classList.add('comment');
+
+  const container = document.createElement('div');
+  container.classList.add('container');
+  commentBlock.appendChild(container);
+
+  const avatar = document.createElement('img');
+  avatar.classList.add('avatar');
+  avatar.src = comment.pathToUserImg;
+  container.appendChild(avatar);
+
+  const commentContent = document.createElement('div');
+  commentContent.classList.add('commentContent');
+  container.appendChild(commentContent);
+
+  const userName = document.createElement('h3');
+  userName.classList.add('userName');
+  userName.textContent = comment.userName;
+  commentContent.appendChild(userName);
+
+  const commentDate = document.createElement('p');
+  commentDate.classList.add('commentDate');
+  commentDate.textContent = comment.date.toLocaleDateString('ru-RU');
+  commentContent.appendChild(commentDate);
+
+  const commentText = document.createElement('p');
+  commentText.classList.add('commentText');
+  commentText.textContent = comment.description;
+  commentContent.appendChild(commentText);
+
+  if (comment.children) {
+    commentBlock.classList.add('hasInvis');
+    const subBlock = document.createElement('article');
+    subBlock.classList.add('subCommentContainer');
+    subBlock.classList.add('invisible');
+    commentBlock.appendChild(subBlock);
+    comment.children.forEach(x => {
+      subBlock.appendChild(createComment(x));
+    });
+    container.addEventListener('click', () => {
+      subBlock.classList.toggle('invisible');
+      commentBlock.classList.toggle('hasInvis');
+    });
+  }
+
+  return commentBlock;
+}
+
+let start = 0;
+let counter = 0;
+function displayTenComments(start, post) {
+  let end = start + 10;
+  if (post.comments.length < end){
+    end = post.comments.length;
+  }
+  for (let i = start; i < end; i++, counter++) {
+    document.querySelector('.commentContainer').appendChild(createComment(post.comments[i]));
+  }
+  start = end + 1;
+}
+
+
+displayTenComments(start, post);
+
