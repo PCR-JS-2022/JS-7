f78ec83e9096e532b61b048695defe135d88d229
Miropolsky <Miropolskysk@yandex.ru>
2022-04-28 10:36:17 +0500

    first

---
+++ src/srcipt.js
@@ -0,0 +1,130 @@
+const cardImg = document.querySelector('.card_img');
+const infTitle = document.querySelector('.information_title');
+const description = document.querySelector('.description');
+const date = document.querySelector('.date');
+const tags = document.querySelector('.tags');
+const likesIcon = document.querySelector('.love-icon');
+const likes = document.querySelector('.likes');
+const countComments = document.querySelector('.count-com');
+const listComments = document.querySelector('.list-comment');
+let counCom = 0;
+let curIndex = 0;
+
+
+function formatDate(date) {
+    let dd = date.getDate();
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    let mm = date.getMonth() + 1;
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+    return dd + '.' + mm + '.' + date.getFullYear();
+}
+
+function addComment(start) {
+    let count = 0;
+    for (let i = start; i < start + 5; i++) {
+        if (count >= 5) {
+            continue;
+        }
+        if (i > post.comments.length - 1) {
+            break;
+        }
+        let curCom = post.comments[i];
+        let li = document.createElement('li');
+        li.classList.add('commentLi');
+        if (curCom.hasOwnProperty('children')) {
+            li.innerHTML += `<div class="comment" style="background-color:ivory;">` + `<img class="comment_img" src="${curCom.pathToUserImg}">`
+                + '<div class="comment_text">' + `<p>${curCom.userName}</p> <p>${formatDate(curCom.date)}</p> <p>${curCom.description}</p>`
+                + `</div> </div>`;
+            curIndex++;
+            count++;
+            counCom++;
+            let ul = document.createElement('ul');
+            ul.classList.add('close');
+            let coundChild = 0;
+            for (let j = 0; j < curCom.children.length; j++) {
+                let curChild = curCom.children[j];
+                ul.innerHTML += `<li class="commentLi"> <div class="comment">`
+                    + `<img class="comment_img" src="${curChild.pathToUserImg}">`
+                    + '<div class="comment_text">' + `<p>${curChild.userName}</p> <p>${formatDate(curChild.date)}</p> <p>${curChild.description}</p>`
+                    + `</div> </div> </li> </ul>`;
+                coundChild++;
+            }
+            li.addEventListener("click", () => {
+                if (ul.classList.contains('close')) {
+                    ul.classList.remove('close');
+                    li.append(ul);
+                    counCom += coundChild;
+                    countComments.innerHTML = counCom;
+                }
+                else {
+                    li.removeChild(li.lastChild);
+                    ul.classList.add('close');
+                    counCom -= coundChild;
+                    countComments.innerHTML = counCom;
+                }
+            });
+        } else {
+            li.innerHTML += `<div class="comment">` + `<img class="comment_img" src="${curCom.pathToUserImg}">`
+                + '<div class="comment_text">' + `<p>${curCom.userName}</p> <p>${formatDate(curCom.date)}</p> <p>${curCom.description}</p>`
+                + `</div> </div>`;
+            curIndex++;
+            count++;
+            counCom++;
+        }
+        li.innerHTML += "</li>";
+        listComments.append(li);
+    }
+    countComments.innerHTML = counCom;
+}
+
+cardImg.src = post.pathToPostImg;
+infTitle.innerHTML = post.title;
+description.innerHTML = post.description;
+date.innerHTML = formatDate(post.date);
+
+post.tags.forEach(el => {
+    tags.innerHTML += `<p>${el}</p>`;
+});
+likes.innerHTML = post.likes;
+listComments.innerHTML = "";
+addComment(curIndex);
+
+likesIcon.addEventListener('click', () => {
+    if (likesIcon.classList.contains('liked')) {
+        likesIcon.classList.remove('liked');
+        likes.textContent--;
+    }
+    else {
+        likes.textContent++;
+        likesIcon.classList.add('liked');
+    }
+});
+
+let prevPosition = window.scrollY;
+
+window.addEventListener('scroll', () => {
+
+    let windowRelative = document.documentElement.getBoundingClientRect();
+    let scroll = window.scrollY;
+
+    if (windowRelative.bottom < document.documentElement.clientHeight + 100) {
+        //Удаление первых комментариев при прокрутке вниз
+        /*while (counCom > 50) {
+            window.scrollBy(0, -105);
+            listComments.firstChild.remove();
+            counCom--;
+            countComments.innerHTML = counCom;
+        }*/
+        addComment(curIndex);
+    }
+    if (scroll < prevPosition) {
+        console.log("up");
+    }
+    prevPosition = scroll;
+});
+
+
